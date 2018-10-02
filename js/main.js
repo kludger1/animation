61b2ea092784c2d6a500593481377ddbc1d6f379
unknown <ludgerk@techlaunch.com>
2018-10-02 13:42:47 -0400

    took everything out

--- js/main.js
+++ js/main.js
@@ -1,28 +1,27 @@
-const boy = document.getElementById('boy')
-const dog = document.getElementById('dog')
-const bird = document.getElementById('bird')
-const instuctions = document.getElementById('instuctions')
-const gn = document.getElementById('gn')
+// const boy = document.getElementById('boy')
+// const dog = document.getElementById('dog')
+// const bird = document.getElementById('bird')
+// const instuctions = document.getElementById('instuctions')
+// const gn = document.getElementById('gn')
 
 
-$(window).keydown(function (e) {
-    if (e.keyCode === 39) {
-        instuctions.style.visibility = 'hidden'
-        boy.style.animationName = 'boyRun'
-        dog.style.animationName = 'dogRun'
-        bird.style.visibility = 'visible'
-        boy.style.transform = 'translate(900px, -80px)';
-        dog.style.transform = 'translate(800px, -100px)';
-        bird.style.transform = 'translate(-1600px, -350px)';
-        gn.style.transform = 'translateY(200px) rotateX(360deg) scale(2)'
+// $(window).keydown(function (e) {
+//     if (e.keyCode === 39) {
+//         boy.style.animationName = 'boyRun'
+//         dog.style.animationName = 'dogRun'
+//         bird.style.visibility = 'visible'
+//         boy.style.transform = 'translate(900px, -80px)';
+//         dog.style.transform = 'translate(800px, -100px)';
+//         bird.style.transform = 'translate(-1600px, -350px)';
+//         gn.style.transform = 'translateY(200px) rotateX(360deg) scale(2)'
 
 
-    }
-
-
+//     }
 
 
 
-})
+
+
+// })
 
 
